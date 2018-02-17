fff8f9dde5f934cde4f8a3975708cd1406dd156b
Rachel Fish <itsrachelfish@gmail.com>
2018-02-17 16:25:23 -0700

    Basic mempool data source from Eth Gas Station

--- data-sources/mempool.js
+++ data-sources/mempool.js
@@ -1,10 +1,31 @@
 // File for getting data from the mempool
 const request = require('superagent');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const cheerio = require('cheerio');
+const cheerioTableparser = require('cheerio-tableparser');
 
 request.get('https://ethgasstation.info/txPoolReport.php').end((error, response) =>
 {
-    const dom = new JSDOM(response.text);
-    console.log("Block #" + dom.window.document.querySelector('.x_title span').textContent);
+    const $ = cheerio.load(response.text);
+    const blockNumber = $('.x_title span').text();
+
+    cheerioTableparser($);
+    const mempool = $(".x_content table").parsetable();
+    const stats = [];
+    let total = 0;
+
+    console.log("Block #" + blockNumber);
+
+    for(var i = mempool[0].length - 1; i > 0; i--)
+    {
+        // ETH Gas Station returns the total in the pool at or above specific gradiations, so we have to subtract the total from the current value
+        const priceCount = parseInt(mempool[3][i]) - total;
+
+        if(priceCount > 0)
+        {
+            stats.push({'gasPrice': mempool[0][i], 'count': priceCount});
+            total += priceCount;
+        }
+    }
+
+    console.log({'block': blockNumber, 'totalPending': total, 'mempool': stats});
 });
